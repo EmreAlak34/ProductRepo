d6e070d61d3926ed5be006016609bdf9d9a5bfc7
Emre Alak <alakemre@gmail.com>
2024-12-10 09:36:53 +0100

    Add order-status

--- src/main/java/org/example/ShopService.java
+++ src/main/java/org/example/ShopService.java
@@ -30,7 +30,7 @@ public class ShopService {
         double totalPrice = orderedProducts.stream().mapToDouble(Product::getPrice).sum();
 
         // Create  order and add it to orderRepo
-        Order order = new Order(orderId, orderedProducts, totalPrice);
+        Order order = new Order(orderId, orderedProducts, totalPrice,orderStatus);
         orderRepo.addOrder(order);
 
         // Print message
